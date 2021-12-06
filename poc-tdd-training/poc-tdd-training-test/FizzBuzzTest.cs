965870aa2987aa146e83630eadd889fbfdb88c0f
bory-kb <s.kubo0527@gmail.com>
2021-12-06 14:34:54 +0900

    init

---
+++ poc-tdd-training/poc-tdd-training-test/FizzBuzzTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace poc_tdd_training_test
+{
+    public class FizzBuzzTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ poc-tdd-training/poc-tdd-training/FizzBuzz.cs
@@ -0,0 +1,7 @@
+﻿class FizzBuzz
+{
+    public static void Main()
+    {
+        Console.WriteLine("aaa");
+    }
+}
