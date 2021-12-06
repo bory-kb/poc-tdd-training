620ebffe2ed2f109ebcb7f8d6a2c229d572adcd1
bory-kb <s.kubo0527@gmail.com>
2021-12-06 15:49:59 +0900

    6のリファクタOK

--- poc-tdd-training/poc-tdd-training-test/FizzBuzzTest.cs
+++ poc-tdd-training/poc-tdd-training-test/FizzBuzzTest.cs
@@ -17,28 +17,24 @@ namespace poc_tdd_training_test
 
         public void ”š1‚ğ“n‚µ‚Ä•¶š—ñ1‚ğ•Ô‚·()
         {
-            string ret = _fizzBuzz.Convert(1);
-            Assert.AreEqual("1", ret);
+            Assert.AreEqual("1", _fizzBuzz.Convert(1));
         }
         [Test]
         public void ”š2‚ğ“n‚µ‚Ä•¶š—ñ2‚ğ•Ô‚·()
         {
-            string ret = _fizzBuzz.Convert(2);
-            Assert.AreEqual("2", ret);
+            Assert.AreEqual("2", _fizzBuzz.Convert(2));
         }
 
         [Test]
         public void ”š3‚ğ“n‚µ‚Ä”š‚Ì‘ã‚í‚è‚ÉFizz‚ğ•Ô‚·()
         {
-            string ret = _fizzBuzz.Convert(3);
-            Assert.AreEqual("Fizz", ret);
+            Assert.AreEqual("Fizz", _fizzBuzz.Convert(3));
         }
 
         [Test]
         public void ”š6‚ğ“n‚µ‚Ä”š‚Ì‘ã‚í‚è‚ÉFizz‚ğ•Ô‚·()
         {
-            string ret = _fizzBuzz.Convert(6);
-            Assert.AreEqual("Fizz", ret);
+            Assert.AreEqual("Fizz", _fizzBuzz.Convert(6));
         }
     }
 }
